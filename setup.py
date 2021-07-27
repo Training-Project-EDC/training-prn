bf59b40d4b92eb64624bb19eeafaf6ff02a9472e
Moses Chawawa <moseschawawa@gmail.com>
2021-07-27 12:04:33 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ setup(
     author_email='se-dmc@bhp.org.bw',
     packages=find_packages(),
     include_package_data=True,
-    url='https://github.com/covid19-vaccine/esr21-prn',
+    url='',
     license='GPL license, see LICENSE',
     description='training prn.',
     long_description=README,
